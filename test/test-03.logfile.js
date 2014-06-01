99ca12f9d4a5bbb7b9fa29eb07bf81701171b429
C J Silverio <ceej@npmjs.com>
2014-05-31 21:16:44 -0400

    A working logfile metrics output & tests for same.

--- test/test-03.logfile.js
+++ test/test-03.logfile.js
@@ -1,13 +1,96 @@
 'use strict';
 
 var
-    lab         = require('lab'),
-    describe    = lab.describe,
-    it          = lab.it,
-    demand      = require('must')
+    lab       = require('lab'),
+    describe  = lab.describe,
+    it        = lab.it,
+    demand    = require('must'),
+    bole      = require('bole'),
+    fs        = require('fs'),
+    LogOutput = require('../lib/output-logfile'),
+    mkdirp    = require('mkdirp'),
+    path      = require('path'),
+    rimraf    = require('rimraf')
     ;
+
+var tmpdir = './tmp';
 
 describe('logfile output', function()
 {
-    it('should have tests');
+    var output;
+    var mockopts = { path: path.join(tmpdir, 'foo.log'), name: 'test-1' };
+
+    lab.before(function(done)
+    {
+        mkdirp(tmpdir, done);
+    });
+
+    it('demands an options object', function(done)
+    {
+        function shouldThrow() { return new LogOutput(); }
+        shouldThrow.must.throw(/options/);
+        done();
+    });
+
+    it('demands a path object', function(done)
+    {
+        function shouldThrow() { return new LogOutput({}); }
+        shouldThrow.must.throw(/path/);
+        done();
+    });
+
+    it('demands a name object', function(done)
+    {
+        function shouldThrow() { return new LogOutput({ path: '../tmp'}); }
+        shouldThrow.must.throw(/name/);
+        done();
+    });
+
+    it('can be constructed', function(done)
+    {
+        output = new LogOutput(mockopts);
+        output.must.be.an.object();
+        output.must.be.instanceof(LogOutput);
+        done();
+    });
+
+    it('creates a logger client', function(done)
+    {
+        output.must.have.property('client');
+        output.client.must.be.truthy();
+        output.client.must.have.property('info');
+        output.client.info.must.be.a.function();
+        done();
+    });
+
+    it('emits to its logfile', function(done)
+    {
+        output.write({ test: 'yes'}, function()
+        {
+            fs.readFile(mockopts.path, function(err, data)
+            {
+                data = data.toString('utf8');
+                var first = data.split('\n')[0];
+                var written = JSON.parse(first);
+                written.must.be.an.object();
+                written.level.must.equal('info');
+                written.name.must.equal('test-1');
+                written.test.must.equal('yes');
+                done();
+            });
+        });
+    });
+
+    it('has a useful toString() implementation', function(done)
+    {
+        var str = output.toString();
+        str.must.equal('[ logfile @ tmp/foo.log ]');
+        done();
+    });
+
+    lab.after(function(done)
+    {
+        rimraf(tmpdir, done);
+    });
+
 });
