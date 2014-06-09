051d59edc51ffaf770f42392d4f3ee7cb26a07ec
C J Silverio <ceej@npmjs.com>
2014-06-08 19:26:47 -0700

    Remove the examples directory: the emitter repo has a better one.

--- examples/config.example.js
+++
@@ -1,4 +0,0 @@
-module.exports =
-{
-    // example configuration goes here
-};
--- examples/process.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-// TODO
-// a simple http server that emits metrics when it serves a page
-// also sends a heartbeat on a timer
