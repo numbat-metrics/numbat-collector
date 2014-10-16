ef7612dc3ba6484e53c9450569b541e37931e7f8
C J Silverio <ceej@npmjs.com>
2014-10-16 15:05:33 -0700

    A little groundskeeping.
    Bumped version of json-stream to pick up a bugfix.
    Bumped other dep versions.
    Cleaned up logger configuration; why pass an object around?
    Brought test coverage up to around 70%, which isn't very good.

--- bin/run-server.js
+++ bin/run-server.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 var
-    Numbat       = require('../index'),
-    createLogger = require('../lib/logging')
-    path         = require('path')
+    Numbat  = require('../index'),
+    logging = require('../lib/logging'),
+    path    = require('path')
     ;
 
 var config = require(path.resolve(process.argv[2]));
-config.log = createLogger(config.logging);
+logging(config.logging);
 
 var server = new Numbat(config);
 server.listen();
---
+++ test/test-01-collector.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var
+    lab       = require('lab'),
+    describe  = lab.describe,
+    it        = lab.it,
+    demand    = require('must'),
+    Numbat = require('../index')
+    ;
+
+describe('collector', function()
+{
+    describe('exports', function()
+    {
+        it('exports a function', function(done)
+        {
+            Numbat.must.be.a.function();
+            done();
+        });
+
+        it('exports the three outputs as well', function(done)
+        {
+            Numbat.must.have.property('InfluxOutput');
+            Numbat.InfluxOutput.must.be.a.function();
+            Numbat.must.have.property('LogOutput');
+            Numbat.LogOutput.must.be.a.function();
+            Numbat.must.have.property('AnalyzerOutput');
+            Numbat.AnalyzerOutput.must.be.a.function();
+
+            done();
+        });
+    });
+
+    describe('constructor', function()
+    {
+        var goodOpts =
+        {
+            listen: { path: '/tmp/numbat.sock' },
+            outputs: []
+        };
+
+        it('requires an options object', function(done)
+        {
+            function shouldThrow() { return new Numbat(); }
+            shouldThrow.must.throw('you must pass an options object');
+            done();
+        });
+
+        it('requires a listen option', function(done)
+        {
+            function shouldThrow() { return new Numbat({}); }
+            shouldThrow.must.throw(/listen/);
+            done();
+        });
+
+        it('requires an outputs option', function(done)
+        {
+            function shouldThrow() { return new Numbat({ listen: { path: '/tmp/numbat.sock' }}); }
+            shouldThrow.must.throw(/array of clients/);
+            done();
+        });
+
+        it('can be constructed', function(done)
+        {
+            var collector = new Numbat(goodOpts);
+            collector.must.be.instanceof(Numbat);
+
+            done();
+        });
+
+    });
+
+    describe('listen()', function()
+    {
+        it('should have tests');
+    });
+
+});
