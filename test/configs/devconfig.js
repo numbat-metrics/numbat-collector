73b4237964b3acd5996286819f0906a47cd2e59f
C J Silverio <ceejceej@gmail.com>
2015-07-13 15:08:15 -0700

    `npm run dev` now listens on tcp & doesn't send to influx.

--- test/configs/devconfig.js
+++ test/configs/devconfig.js
@@ -3,14 +3,12 @@ module.exports =
 	logging:
 	{
 		name: 'numbat-1',
-		silent: false,
+		silent: true,
 	},
-	listen: { path: '/tmp/numbat.sock' },
+	listen: { port: 4677 },
 	outputs:
 	[
 		{ type: 'analyzer',  host: 'localhost', port: 3337 },
-		{ type: 'log', name: 'numbat-1', path: './numbat.log' },
-		{ type: 'influxdb', hosts: [ { host: 'localhost',  port: 8086 }],
-			username: 'numbat', password: 'my-top-secret', database: 'numbat' }
+		{ type: 'prettylog', name: 'numbat-1', pipe: true },
 	]
 };
