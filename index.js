915067f495f52627832ba45c7f6a93e2ad15c5e4
Trygve Lie <github@trygve-lie.com>
2016-02-07 01:35:41 +0100

    strict mode, arrow functions, const and let instead of var, correct octal literals when in es6

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var Numbat            = require('./lib/collector');
+"use strict";
+
+const Numbat          = require('./lib/collector');
 Numbat.InfluxOutput   = require('./lib/output-influx');
 Numbat.LogOutput      = require('./lib/output-logfile');
 Numbat.AnalyzerOutput = require('./lib/output-analyzer');
